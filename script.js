1ca0e9fafda93db216bc39a0394effc023e675bc
Anneice Manzanares <mountains.stars@gmail.com>
2023-02-10 19:05:26 -0700

    delete touch support that didn't work

--- script.js
+++ script.js
@@ -155,27 +155,3 @@ function setDefaults(){
 }
 
 window.onload= setDefaults;
-
-document.addEventListener("touchstart", touch2Mouse, true);
-document.addEventListener("touchmove", touch2Mouse, true);
-document.addEventListener("touchend", touch2Mouse, true);
-
-function touch2Mouse(e)
-{
-  let theTouch = e.changedTouches[0];
-  let mouseEv;
-
-  switch(e.type)
-  {
-    case "touchstart": mouseEv="mousedown"; break;  
-    case "touchend":   mouseEv="mouseup"; break;
-    case "touchmove":  mouseEv="mousemove"; break;
-    default: return;
-  }
-
-  let mouseEvent = document.createEvent("MouseEvent");
-  mouseEvent(mouseEv, true, true, window, 1, theTouch.screenX, theTouch.screenY, theTouch.clientX, theTouch.clientY, false, false, false, false, 0, null);
-  theTouch.target.dispatchEvent(mouseEvent);
-
-  e.preventDefault();
-}
